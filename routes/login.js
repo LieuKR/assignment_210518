1fa44cfe52bd53cdea3cdd8483d0f1d6f60d8d6f
LieuKR <korlieu@gmail.com>
2021-05-18 23:38:22 +0900

    jwt 토큰 생성 및 검증 확인 완료

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,7 @@ const cryptoconfig  = require('../config/pwcryptset.json');
 
 // jsonwebtoken 모듈
 const jwt = require('jsonwebtoken')
+const jwtconfig  = require('../config/jwt_secret.json');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
@@ -21,9 +22,15 @@ router.post('/', function(req, res, next) {
       MySqlHandler.DB.query(`SELECT * FROM users WHERE id='${req.body.id}' and password='${derivedKey.toString('hex')}'`,
       (err, rows) => {
         if (rows[0] == null) {
+          // 이 경우 아이디, 비밀번호가 잘못되었으므로 그냥 메인화면으로 보냄
           console.log('아이디 혹은 비밀번호가 잘못되었습니다');
         } else {
-          console.log('로그인 되었습니다');
+          // 이 경우 jwt를 발급
+          var token = jwt.sign({test : 'test1', testtest : 'testets'}, `${jwtconfig.secret}`);
+          console.log(token)
+          console.log('로그인 되었습니다. jst를 검증합니다.');
+          var decoded_data = jwt.verify(token, `${jwtconfig.secret}`);
+          console.log(decoded_data)
         }
       });
     })
