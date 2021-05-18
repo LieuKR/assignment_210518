9bc8d1f56341b5b49017246227dde61d339a3313
LieuKR <korlieu@gmail.com>
2021-05-19 08:40:13 +0900

    좋아요 처리 완료. 사용자에 따라 한 글에 좋아요, 싫어요 하나만 할 수 있게 할 것

--- routes/comments.js
+++ routes/comments.js
@@ -50,17 +50,30 @@ router.post('/add_good', function(req, res, next) {
         // up_down_check DB에 이미 좋아요/싫어요 한 기록이 있는지 체크
         MySqlHandler.DB.query(`SELECT * FROM \`up_down_check\` where (target = ${req.body.no} and user = '${res.locals.loginid}')`,
         (err, rows) => {
-          // DB에 좋아요/싫어요 한 기록이 존재할 경우
-          if(rows[0].value){
-            console.log(rows.value)  
-            res.redirect('back');
-          } else { // DB에 좋아요/싫어요 한 기록이 없을 경우
-            MySqlHandler.DB.query(`INSERT INTO \`up_down_check\` (user, value, target) VALUES ('${res.locals.loginid}', 1, '${req.body.no}');
-            UPDATE \`comments\` SET good_num = good_num + 1 WHERE (no = ${req.body.no});`,
-                (err, rows) => {
-                    console.log('좋아요 되었습니다')
-                    res.redirect('back');
-                })
+            if(rows[0] == undefined) { // DB에 좋아요/싫어요 한 기록이 없을 경우 => 좋아요 추가
+                MySqlHandler.DB.query(`INSERT INTO \`up_down_check\` (user, value, target) VALUES ('${res.locals.loginid}', 1, '${req.body.no}');
+                UPDATE \`comments\` SET good_num = good_num + 1 WHERE (no = ${req.body.no});`,
+                    (err, rows) => {
+                        if(err) throw(err);
+                        console.log('좋아요 되었습니다.')
+                        res.redirect('back');
+                    })
+            } else if(rows[0].value == 1){ // DB에 좋아요 했던 기록이 존재할 경우 => 좋아요 취소
+                MySqlHandler.DB.query(`DELETE FROM \`up_down_check\` WHERE (target = ${req.body.no} and user = '${res.locals.loginid}');
+                UPDATE \`comments\` SET good_num = good_num - 1 WHERE (no = ${req.body.no});`,
+                    (err, rows1) => {
+                        if(err) throw(err);
+                        console.log('좋아요가 취소되었습니다.')
+                        res.redirect('back');
+                    })
+            } else if(rows[0].value == 2){ // DB에 싫어요 했던 기록이 존재할 경우 => 싫어요 취소 & 좋아요 추가
+                MySqlHandler.DB.query(`UPDATE \`up_down_check\` SET value = 1 WHERE (target = ${req.body.no} and user = '${res.locals.loginid}');
+                UPDATE \`comments\` SET bad_num = bad_num - 1, good_num = good_num + 1 WHERE (no = ${req.body.no});`,
+                    (err, rows1) => {
+                        if(err) throw(err);
+                        console.log('싫어요가 취소되고, 좋아요 되었습니다.')
+                        res.redirect('back');
+                    })
           }
         });
     } else { // 로그인이 안되어있을 경우
