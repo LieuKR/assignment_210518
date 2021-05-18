12fff08674e28fc842a0322644be9dc1046231ce
LieuKR <korlieu@gmail.com>
2021-05-19 06:43:07 +0900

    DB에서 댓글 내용 가져와 출력되게끔 처리

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+// Mysql DB와 관련된 부분 모듈화
+const MySqlHandler = require('../serverside_functions/MySqlHandler.js');  
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(res.locals.loginid){
     res.redirect('/comments')
   } else {
-    res.render('index', { title: '메인' });
+    res.render('index', {title: '메인' });
   }
 });
 
 router.get('/comments', function(req, res, next) {
   if(res.locals.loginid){
-    res.render('comments', { title: '댓글' });
+    MySqlHandler.DB.query(`SELECT * FROM \`comments\` ORDER BY \`no\` ASC`,
+      (err, rows) => {
+        res.render('comments', {title: '댓글', comments : rows});
+      });
   } else {
     res.redirect('/')
   }
