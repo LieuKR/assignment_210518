082a3611dea032d37905e990d01cff99cc8083a8
LieuKR <korlieu@gmail.com>
2021-05-18 22:52:02 +0900

    입력된 post데이터 출력확인

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
+  console.log(req.body.id)
+  console.log(req.body.password)
   res.send('respond with a resource');
 });
 
