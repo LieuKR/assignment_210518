daee64886d7b283077bfec70392c0b0a168e19d9
LieuKR <korlieu@gmail.com>
2021-05-19 08:03:21 +0900

    좋아요 처리 작성중, DB에 좋아요/싫어요 한 기록이 없을 경우 comments의 좋아요 개수 +1 & 좋아요/싫어요 기록에 추가

--- routes/comments.js
+++ routes/comments.js
@@ -47,10 +47,21 @@ router.post('/delete', function(req, res, next) {
 // 좋아요 처리 파트
 router.post('/add_good', function(req, res, next) {
     if(res.locals.loginid){ // 로그인 되어있을 경우
-      MySqlHandler.DB.query(`DELETE FROM \`comments\` WHERE (no = ${req.body.no} and writer = '${res.locals.loginid}')`,
+        // up_down_check DB에 이미 좋아요/싫어요 한 기록이 있는지 체크
+        MySqlHandler.DB.query(`SELECT * FROM \`up_down_check\` where (target = ${req.body.no} and user = '${res.locals.loginid}')`,
         (err, rows) => {
-          if(err) {throw err}
-          res.redirect('back');
+          // DB에 좋아요/싫어요 한 기록이 존재할 경우
+          if(rows[0].value){
+            console.log(rows.value)  
+            res.redirect('back');
+          } else { // DB에 좋아요/싫어요 한 기록이 없을 경우
+            MySqlHandler.DB.query(`INSERT INTO \`up_down_check\` (user, value, target) VALUES ('${res.locals.loginid}', 1, '${req.body.no}');
+            UPDATE \`comments\` SET good_num = good_num + 1 WHERE (no = ${req.body.no});`,
+                (err, rows) => {
+                    console.log('좋아요 되었습니다')
+                    res.redirect('back');
+                })
+          }
         });
     } else { // 로그인이 안되어있을 경우
       res.redirect('/')
