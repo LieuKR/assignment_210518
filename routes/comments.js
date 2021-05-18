28f08b52ecf5c79440d823347c8273e398736ad0
LieuKR <korlieu@gmail.com>
2021-05-19 06:46:33 +0900

    form에 붙은 쓸데없는 클래스 제거 및 댓글 수정용 form 생성

--- routes/comments.js
+++ routes/comments.js
@@ -7,16 +7,28 @@ const MySqlHandler = require('../serverside_functions/MySqlHandler.js');
 // 댓글 작성하는 파트
 router.post('/write', function(req, res, next) {
   if(res.locals.loginid){ // 로그인 되어있을 경우
-
     MySqlHandler.DB.query(`INSERT INTO \`comments\` (writer, contents, good_num, bad_num) VALUES ('${res.locals.loginid}', '${req.body.comment}', 0, 0)`,
       (err, rows) => {
         if(err) {throw err}
         res.redirect('back');
       });
-
   } else { // 로그인이 안되어있을 경우
     res.redirect('/')
   }
 });
+
+// 댓글 수정 파트
+router.post('/update', function(req, res, next) {
+    if(res.locals.loginid){ // 로그인 되어있을 경우
+      MySqlHandler.DB.query(`INSERT INTO \`comments\` (writer, contents, good_num, bad_num) VALUES ('${res.locals.loginid}', '${req.body.comment}', 0, 0)`,
+        (err, rows) => {
+          if(err) {throw err}
+          res.redirect('back');
+        });
+    } else { // 로그인이 안되어있을 경우
+      res.redirect('/')
+    }
+  });
+  
 
 module.exports = router;
