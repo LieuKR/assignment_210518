dc90a38ddffa90ba8acaa464acaf97f4dc6a5506
LieuKR <korlieu@gmail.com>
2021-05-18 23:21:26 +0900

    비밀번호 암호화 모듈 사용 및 DB에 같은 아이디, 비밀번호가 존재하는지 확인하고 출력하는 쿼리문 작성 및 확인

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+// Mysql DB와 관련된 부분 모듈화
 const MySqlHandler = require('../serverside_functions/MySqlHandler.js');  
+
+// 암호화를 위한 모듈 및 암호화를 위한 값
+const crypto = require('crypto');
+const cryptoconfig  = require('../config/pwcryptset.json');
+
+// jsonwebtoken 모듈
+const jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   console.log(req.body.id)
   console.log(req.body.password)
-  MySqlHandler.DB.query(`SELECT * FROM users`,
+
+  crypto.pbkdf2(req.body.password, cryptoconfig.salt, cryptoconfig.runnum, cryptoconfig.byte, 
+    cryptoconfig.method, (err, derivedKey) => {
+      MySqlHandler.DB.query(`SELECT * FROM users WHERE id='${req.body.id}' and password='${derivedKey.toString('hex')}'`,
       (err, rows) => {
-        console.log('test');
+        if (rows[0] == null) {
+          console.log('아이디 혹은 비밀번호가 잘못되었습니다');
+        } else {
+          console.log('로그인 되었습니다');
+        }
       });
+    })
+
   res.send('respond with a resource');
 });
 
